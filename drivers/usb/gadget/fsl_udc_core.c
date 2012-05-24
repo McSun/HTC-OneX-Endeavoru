43801f2bcca01fb7a24e4b24dfcc102e17a99d0f
riemervdzee <riemervdzee@gmail.com>
2012-05-24 20:57:14 +0200

    gadget/fsl_udc_core.c: Fixes two bugs and warnings. Related to USB
    From top to bottom: (war = warning)
    - war: global_wakeup_state: added int as return value.
    - Bug: fsl_ep_dequeue: struct fsl_req *req was never assigned but was used in the code
    - War: udc_test_mode: IO_USB_PHYS = 0x7D000000. Instead of strange *void pointers (they should be integers anyway), use the proper defines
    - Bug: usb_vbus_state_work: they forgot the ".work" part on this one..
    Plus, A lot of unused variables and mixed code/variables etc. like usual
    
    Signed-off-by: Riemer van der Zee <riemervdzee@gmail.com>

--- drivers/usb/gadget/fsl_udc_core.c
+++ drivers/usb/gadget/fsl_udc_core.c
@@ -90,7 +90,7 @@
 
 #define VUBS_IRQ -22
 #define VBUS_WAKEUP_ENR 19
-extern global_wakeup_state;
+extern int global_wakeup_state;
 
 static int irq_udc_debug;
 int irq_otg_debug;
@@ -1121,7 +1121,7 @@ err_unmap:
 static int fsl_ep_dequeue(struct usb_ep *_ep, struct usb_request *_req)
 {
 	struct fsl_ep *ep = container_of(_ep, struct fsl_ep, ep);
-	struct fsl_req *req;
+	struct fsl_req *req = container_of(_req, struct fsl_req, req);
 	unsigned long flags;
 	int ep_num, stopped, ret = 0;
 	u32 epctrl;
@@ -1670,7 +1670,6 @@ static void udc_test_mode(struct fsl_udc *udc, u32 test_mode)
 	struct fsl_ep *ep;
 	u32 portsc, bitmask;
 	unsigned long timeout;
-	void __iomem *base = 0x7D000000;
 	u32 val;
 
 	/* Ack the ep0 IN */
@@ -1701,10 +1700,10 @@ static void udc_test_mode(struct fsl_udc *udc, u32 test_mode)
 		VDBG("TEST_K\n");
 		break;
 	case PORTSCX_PTC_SEQNAK:
-		val = readl(IO_ADDRESS(base + UTMIP_HSRX_CFG1));
+		val = readl(IO_ADDRESS( IO_USB_PHYS + UTMIP_HSRX_CFG1));
 		val &= ~UTMIP_HS_SYNC_START_DLY(~0);
 		val |= UTMIP_HS_SYNC_START_DLY(0x2);
-		writel(val, IO_ADDRESS(base + UTMIP_HSRX_CFG1));
+		writel(val, IO_ADDRESS( IO_USB_PHYS + UTMIP_HSRX_CFG1));
 		VDBG("TEST_SE0_NAK\n");
 		break;
 	case PORTSCX_PTC_PACKET:
@@ -2294,7 +2293,7 @@ static void fsl_udc_charger_detect_work(struct work_struct* work)
  */
 static void fsl_udc_restart(struct fsl_udc *udc)
 {
-	unsigned long flags = 0;
+	//unsigned long flags = 0; /* htc */
 	USB_INFO("fsl_udc_restart");
 	/* setup the controller in the device mode */
 	dr_controller_setup(udc);
@@ -2583,7 +2582,6 @@ int usb_register_notifier(struct t_usb_status_notifier *notifier)
 
 static void charger_detect_gpio(struct fsl_udc *udc)
 {
-	printk("charger_detect_gpio \n");
 	int val, val1, val2;
 	int charger_type;
 	int board_id = 0;
@@ -2592,6 +2590,9 @@ static void charger_detect_gpio(struct fsl_udc *udc)
 	u32 portsc;
 	int ret;
 	uint8_t command[2]={0};
+
+	printk("charger_detect_gpio \n");
+
 	mdelay(10);
 	board_id = htc_get_pcbid_info();
 	portsc = fsl_readl(&dr_regs->portsc1);
@@ -2965,7 +2966,7 @@ static void usb_do_work(struct work_struct *w)
 }
 static void usb_start(struct fsl_udc *udc)
 {
-	unsigned long flags;
+	//unsigned long flags; /* htc */
 	USB_DEBUG("usb_start\n");
 	/*spin_lock_irqsave(&udc->lock, flags);*/ /* htc */
 	udc->flags |= USB_FLAG_START;
@@ -3020,9 +3021,9 @@ static void usb_prepare(struct fsl_udc *udc)
  * */
 static void usb_vbus_state_work(struct work_struct *w)
 {
-	struct fsl_udc *udc = container_of(w, struct fsl_udc, check_vbus_work);
+	struct fsl_udc *udc = container_of(w, struct fsl_udc, check_vbus_work.work);
 	int _vbus;
-	unsigned long flags = 0;
+	//unsigned long flags = 0; /* htc */
 	if(!udc)
 		return;
 	_vbus = vbus_enabled();
@@ -3056,11 +3057,13 @@ void tegra_usb_set_vbus_state(int online)
 {
 	unsigned long flags = 0;
 	struct fsl_udc *udc = udc_controller;
+	int count = 0;
+
 	VDBG("VBUS %s", online ? "on" : "off");
 	USB_INFO("tegra_usb_set_vbus_state %s \n", online ? "on" : "off");
+
 	wake_lock_timeout(&udc_wake_lock2, 1*HZ);
 	usb_check_count--;
-        int count = 0;
 
 	if (udc && udc->transceiver) {
 		if (udc->vbus_active && !online) {
@@ -3259,8 +3262,9 @@ static DEVICE_ATTR(tps_vbus, 0444, show_tps_vbus, NULL);
 
 static ssize_t show_charger(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	USB_INFO("show_charger\n");
 	unsigned length;
+	USB_INFO("show_charger\n");
+
 	if(udc_controller->connect_type == CONNECT_TYPE_USB || udc_controller->connect_type == CONNECT_TYPE_UNKNOWN){
 		length = sprintf(buf, "%d\n", 1);
 	}
@@ -3277,8 +3281,9 @@ static ssize_t show_charger(struct device *dev, struct device_attribute *attr, c
 static ssize_t store_charger(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	USB_INFO("store_charger\n");
 	int state;
+	USB_INFO("store_charger\n");
+
 	sscanf(buf, "%d", &(state));
 	if(state)
 	{
@@ -3719,7 +3724,7 @@ static int __init fsl_udc_probe(struct platform_device *pdev)
 	u32 dccparams;
 #if defined(CONFIG_ARCH_TEGRA)
 	struct resource *res_sys = NULL;
-	struct fsl_usb2_platform_data *pdata = pdev->dev.platform_data;
+	//struct fsl_usb2_platform_data *pdata = pdev->dev.platform_data;
 #endif
 
 	if (strcmp(pdev->name, driver_name)) {
@@ -4029,8 +4034,8 @@ static int fsl_udc_suspend(struct platform_device *pdev, pm_message_t state)
  *-----------------------------------------------------------------*/
 static int fsl_udc_resume(struct platform_device *pdev)
 {
-	unsigned long val;
 	USB_INFO("fsl_udc_resume #0");
+	//unsigned long val;
 	irq_udc_debug =1 ;
 	irq_otg_debug =1 ;
 	if(global_wakeup_state == VBUS_WAKEUP_ENR)
