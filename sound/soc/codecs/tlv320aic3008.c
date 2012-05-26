d525388a80e8c5da76ada5d1336bf503fc2bab4b
TripNRaVeR <noerialexeew@live.nl>
2012-05-26 21:55:05 +0200

    tlv320aic3008.c: fix warning 'spi_aic3008_allow_sleep' defined but not used

--- sound/soc/codecs/tlv320aic3008.c
+++ sound/soc/codecs/tlv320aic3008.c
@@ -607,12 +607,6 @@ static CODEC_SPI_CMD **init_2d_array(int row_sz, int col_sz)
 	return table_ptr;
 }
 
-static void spi_aic3008_allow_sleep(void)
-{
-	wake_unlock(&codec_clk.idlelock);
-	wake_unlock(&codec_clk.wakelock);
-}
-
 static int aic3008_config_ex(CODEC_SPI_CMD *cmds, int size)
 {
 	int i = 0;
